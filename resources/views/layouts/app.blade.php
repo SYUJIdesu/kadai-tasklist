e9fc0a5225f9cb9e784fa1f9132523c713ddbb85
SYUJIdesu <marketsmile1110@gmail.com>
2018-08-27 08:15:47 +0000

    syusei3

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,12 +2,13 @@
 <html>
     <head>
         <meta charset="utf-8">
+        
         <title>Tasklist</title>
+        
+        
     </head>
 
     <body>
-        @include('commons.error_tasks')
-        
-        @yield('content')
+            @yield('content')
     </body>
 </html>
